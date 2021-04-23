370c088b462e53f5fc79063bd7c663ed010a724e
zzisu <i@zzs1.cn>
2021-04-23 22:38:43 +0800

    update

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if [ $CLIENT_ID ] && [ $CLIENT_KEY ]; then
-  echo -n "${CLIENT_ID}-${CLIENT_KEY}" > /hath/data/data/client_login
-else
-  if [ ! -f /hath/data/data/client_login ]; then
+if [ ! -f /hath/data/data/client_login ]; then
+  if [ $CLIENT_ID ] && [ $CLIENT_KEY ]; then
+    mkdir -p /hath/data/data
+    echo -n "${CLIENT_ID}-${CLIENT_KEY}" > /hath/data/data/client_login
+  else
     echo "Login not found, try specify CLIENT_ID and CLIENT_KEY."
     exit 1
   fi
 fi
+
 
 java -jar HentaiAtHome.jar --cache-dir=/hath/data/cache --data-dir=/hath/data/data --download-dir=/hath/data/download --log-dir=/hath/data/log --temp-dir=/hath/data/temp
